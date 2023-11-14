668045500f080ca66fff0cc1040c6eeb406c8d7c
Raphaël Brahier <r.brahier17@gmail.com>
2023-11-14 20:01:35 +0100

    Add logo to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,10 @@
  * Header component
  */
 
+import { NetworkIcon } from "lucide-react";
+import Link from "next/link";
 import NavLink from "./NavLink";
+
 
 /**
  * Header of the app with the name of the app on the left and navigation links on the right.
@@ -21,7 +24,10 @@ export default function Header() {
 
   return (
     <header className='flex justify-between items-center h-14 border-b'>
-      <h1 className='text-xl font-bold tracking-tight'>HRnet</h1>
+      <Link href="/" className='flex items-center gap-1'>
+        <NetworkIcon className='w-4' color='#5fc9f3' />
+        <h1 className='text-xl font-bold tracking-tight'>HRnet</h1>
+      </Link>
       <nav>
         {navLinks.map((link) => (
           <NavLink key={link.title} href={link.href} title={link.title} />
